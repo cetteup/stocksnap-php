c33602426c6652d058a082d73eeb89a9eb98b816
taduuda <taduuda@user210314.de>
2017-01-15 18:36:23 +0100

    Improved response structure
    Response body now only contains photos

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -7,55 +7,55 @@ use GuzzleHttp\Exception\RequestException;
 
 class HttpClient
 {
-  private $client;
+    private $client;
 
-  public function __construct()
-  {
-    $this->client = new Client(['base_uri' => 'https://stocksnap.io/api/']);
-  }
-
-  public function all_photos($order_by = 'date', $sort_order = 'desc', $page = 1)
-  {
-    if (!is_string($order_by) || !is_string($sort_order) || !is_int($page)) return null;
-    $photos = $this->send_request('GET','load-photos/'.$order_by.'/'.$sort_order.'/'.$page);
-
-    return $photos;
-  }
-
-  public function author_photos($author_id, $page = 1)
-  {
-    if (!is_int($author_id) || !is_int($page)) return null;
-    $photos = $this->send_request('GET','author-photos/'.$author_id.'/'.$page);
-
-    return $photos;
-  }
-
-  public function search($terms, $order_by = 'relevance', $sort_order = 'desc', $page = 1)
-  {
-    if (!is_string($terms) || !is_string($order_by) || !is_string($sort_order) || !is_int($page)) return null;
-    $results = $this->send_request('GET','search-photos/'.$terms.'/'.$order_by.'/'.$sort_order.'/'.$page);
-
-    return $results;
-  }
-
-  private function send_request($method,$endpoint)
-  {
-    try {
-      $request = new Request($method,$endpoint);
-      $response = $this->client->send($request);
-    } catch (RequestException $e) {
-      if ($e->hasResponse()) {
-        $response = $e->getResponse();
-        $status = $response->getStatusCode();
-        if ($status == 404) {
-          throw new Exception('404 not found');
-        }
-      }
+    public function __construct()
+    {
+        $this->client = new Client(['base_uri' => 'https://stocksnap.io/api/']);
     }
-    return new ResponseArray(
-      $response->getHeaders(),
-      json_decode($response->getBody(),true)
-    );
-  }
+
+    public function all_photos($order_by = 'date', $sort_order = 'desc', $page = 1)
+    {
+        if (!is_string($order_by) || !is_string($sort_order) || !is_int($page)) return null;
+        $photos = $this->send_request('GET','load-photos/'.$order_by.'/'.$sort_order.'/'.$page);
+
+        return $photos;
+    }
+
+    public function author_photos($author_id, $page = 1)
+    {
+        if (!is_int($author_id) || !is_int($page)) return null;
+        $photos = $this->send_request('GET','author-photos/'.$author_id.'/'.$page);
+
+        return $photos;
+    }
+
+    public function search($terms, $order_by = 'relevance', $sort_order = 'desc', $page = 1)
+    {
+        if (!is_string($terms) || !is_string($order_by) || !is_string($sort_order) || !is_int($page)) return null;
+        $results = $this->send_request('GET','search-photos/'.$terms.'/'.$order_by.'/'.$sort_order.'/'.$page);
+
+        return $results;
+    }
+
+    private function send_request($method,$endpoint)
+    {
+      try {
+          $request = new Request($method,$endpoint);
+          $response = $this->client->send($request);
+      } catch (RequestException $e) {
+          if ($e->hasResponse()) {
+              $response = $e->getResponse();
+              $status = $response->getStatusCode();
+              if ($status == 404) {
+                throw new Exception('404 not found');
+              }
+          }
+      }
+      return new ResponseArray(
+          $response->getHeaders(),
+          json_decode($response->getBody(),true)
+      );
+    }
 }
 ?>
--- src/ResponseArray.php
+++ src/ResponseArray.php
@@ -3,44 +3,71 @@ namespace Diza\Stocksnap;
 
 class ResponseArray extends \ArrayObject
 {
-  private $headers;
+    private $headers;
 
-  const NEXT = 'nextPage';
+    const NEXT = 'nextPage';
 
-  const TOTAL = 'count';
-  const PER_PAGE = 40;
+    const TOTAL = 'count';
+    const PER_PAGE = 40;
+    const ORDER_BY = 'sortBy';
+    const SORT_ORDER = 'sortOrder';
 
-  public function __construct($headers, $body)
-  {
-      $this->headers = $headers;
+    public function __construct($headers, $body)
+    {
+        $this->headers = array_merge($headers,$this->extract_header_info($body));
 
-      parent::__construct($body);
-  }
+        parent::__construct($body['results']);
+    }
 
-  public function total_pages()
-  {
-      $total = $this->total_objects();
+    private function extract_header_info($body)
+    {
+        $move_to_header = [];
 
-      return (int) ceil($total / self::PER_PAGE);
-  }
+        if (isset($body[self::ORDER_BY])) {
+            $move_to_header[self::ORDER_BY] = [$body[self::ORDER_BY]];
+        } else {
+            $move_to_header[self::ORDER_BY] = [strtolower(explode('Selected',explode('sidebar','sidebarSortBySelected')[1])[0])];
+        }
 
-  public function total_objects()
-  {
-      $total = 0;
-      if (!empty($this[self::TOTAL]) && is_int($this[self::TOTAL])) {
-          $total = (int) $this[self::TOTAL];
-      }
+        if (isset($body[self::SORT_ORDER])) {
+            $move_to_header[self::SORT_ORDER] = [$body[self::SORT_ORDER]];
+        } else {
+            $move_to_header[self::SORT_ORDER] = [strtolower(explode('Selected',explode('sidebar','sidebarSortOrderSelected')[1])[0])];
+        }
 
-      return $total;
-  }
+        if (isset($body[self::TOTAL])) {
+            $move_to_header[self::TOTAL] = [(int) $body[self::TOTAL]];
+        }
 
-  public function current_page()
-  {
-      if (isset($this[self::NEXT])) {
-          $page = $this[self::NEXT] - 1;
-      }
+        $move_to_header[self::NEXT] = [(int) $body[self::NEXT]];
 
-      return $page;
-  }
+        return $move_to_header;
+    }
+
+    public function total_pages()
+    {
+        $total = $this->total_objects();
+
+        return (int) ceil($total / self::PER_PAGE);
+    }
+
+    public function total_objects()
+    {
+        $total = 0;
+        if (!empty($this->headers[self::TOTAL]) && is_array($this->headers[self::TOTAL])) {
+            $total = (int) $this->headers[self::TOTAL][0];
+        }
+
+        return $total;
+    }
+
+    public function current_page()
+    {
+        if (isset($this->headers[self::NEXT])) {
+            $page = $this->headers[self::NEXT][0] - 1;
+        }
+
+        return $page;
+    }
 }
 ?>
